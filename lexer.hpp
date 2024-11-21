7eba4b5717970f9a1326b360dd04e77d2d0c1930
mannyjimen <manueljimenez0320@gmail.com>
2024-11-20 20:34:42 -0500

    Implemented Token constructor, and various functions for the Lexer

--- lexer.hpp
+++ lexer.hpp
@@ -35,28 +35,57 @@ enum TokenClass{
     ClosePara,
     Operation,
     Equal,
-    OpenQuote,
-    CloseQuote,
+    Quote,
     Colon,
-    NextLine,
-    Space,
+    Invalid,
 };
 
-class Token{
-    public:
-    
-
-    private:
+struct Token{
     char m_token;
     TokenClass m_class;
+    Token(const char& x){
+        m_token = x;
+        if (x >= 48 && x <= 57)
+            m_class = Number;
+        else if ((x >= 65 && x <= 90) || (x >= 97 && x <= 122))
+            m_class = Letter;
+        else if (x == '+' || x == '-' || x == '*' || x == '/')
+            m_class = Operation;
+        else{
+            switch(x){
+                case '=':
+                    m_class = Equal;
+                    break;
+                case '(':
+                    m_class = OpenPara;
+                    break;
+                case ')':
+                    m_class = ClosePara;
+                    break;
+                case 44:
+                    m_class = Quote;
+                    break;
+                case ':':
+                    m_class = Colon;
+                    break;
+                default: 
+                    m_class = Invalid;
+            }
+        }
+    }
 };
 
 class Lexer{
     public:
+    Lexer(const std::string& src_file); //constructing Lexer with src file (text file with code)
+
+    Token createToken(const char& x);
+    std::vector<Token> getTokensForLine(const std::string& line); //return vector of tokens to be pushed to vector
+    std::vector<std::vector<Token>> getAllTokens();
+    void printTokens();
 
     private:
-
-    
+    std::vector<std::vector<Token>> linesOfTokens;  
 };
 
 #endif
