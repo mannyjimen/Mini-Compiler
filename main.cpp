5c721252b1340964e88a61a694e4816adbb21c25
mannyjimen <manueljimenez0320@gmail.com>
2025-05-28 00:43:40 -0400

    added very minor helper function for ExpressionTypes testing. Thinking about creating a decently sized separate file function for testing, not sure since this isn't really a personal project. Very good for learning so will probably end up doing so

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
-#include "Lox.hpp" 
-#include "Token.hpp"
+#include "Lox.hpp"
+#include "ExpressionTypes.hpp"
+
+void testingExpr(){
+    // Token testToken1(TokenType::STRING, "test_token_1", "hello", 5);
+    // Literal testLiteral1(testToken1);
+    // Literal testLiteral2(testToken1);
+
+    // Binary testBinary(testLiteral1, testToken1, testLiteral2);
+    // std::cout << testBinary.m_op.getLexeme();
+}
 
 int main(int argc, char* argv[])
 {
-    //Lox firstProg(argc, argv);
-    // Token testTok(TokenType::LEFT_PAREN, "x", "hello", 5);
-    // std::cout << testTok.toString() << std::endl;
+    Lox firstProg(argc, argv);
 }
