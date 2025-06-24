9d1d19cbb5b8c253ab12fce68e19e87ddf3ba572
mannyjimen <manueljimenez0320@gmail.com>
2025-06-24 17:30:18 -0400

    Began working on evaluating expressions (via the Interpreter), still need to get to know the logic of the format well. Using visitor pattern once again!

---
+++ Interpreter.cpp
@@ -0,0 +1,89 @@
+#include "Interpreter.hpp"
+
+void Interpreter::visit(Literal& literal){
+    m_returns.push(literal.m_lit);
+}
+
+void Interpreter::visit(Grouping& grouping){
+    evaluate(grouping.m_contents);
+}
+
+void Interpreter::visit(Unary& unary){
+    LoxObject right = evaluate(unary.m_operand);
+
+    switch (unary.m_logicalop.m_type){
+        case TokenType::MINUS : 
+            m_returns.push(-(std::get<double>(right)));
+            return;
+        case TokenType::BANG :
+            m_returns.push(!isTruthy(right));
+            return;
+    }
+    m_returns.push("NIL");
+}
+
+LoxObject Interpreter::evaluate(std::shared_ptr<Expr> expr){
+    expr->accept(*this);
+}
+
+bool Interpreter::isTruthy(const LoxObject& obj){
+    if (std::get_if<bool>(&obj)) return std::get<bool>(obj);
+    else if (std::get_if<std::string>(&obj) && std::get<std::string>(obj) == "NIL") return false; 
+    
+    return true;
+}
+
+void Interpreter::visit(Binary& binary){
+    LoxObject left = evaluate(binary.m_left);
+    LoxObject right = evaluate(binary.m_right);
+    //double dleft = std::get<double>(left); double dright = std::get<double>(right);
+    bool areBools, areDoubles;
+    bool b_left, b_right;
+    double d_left, d_right;
+    if (std::get_if<bool>(&left) && std::get_if<bool>(&right)) areBools = true;
+    if (std::get_if<double>(&left) && std::get_if<double>(&right)) areDoubles = true;
+    if(areBools){
+        b_left = std::get<bool>(left); b_right = std::get<bool>(right);
+    }
+    else if(areDoubles){
+        d_left = std::get<double>(left); d_right = std::get<double>(right);
+    }
+
+    switch (binary.m_op.m_type){
+        case TokenType::MINUS:
+            m_returns.push(d_left - d_right); return;
+        case TokenType::SLASH:
+            m_returns.push(d_left / d_right); return;
+        case TokenType::STAR:
+            m_returns.push(d_left * d_right); return;
+        case TokenType::PLUS:
+            if (std::get_if<std::string>(&left) && std::get_if<std::string>(&right)){
+                m_returns.push(std::get<std::string>(left) + std::get<std::string>(right)); return;
+            }
+            if (areDoubles){
+                m_returns.push(d_left + d_right); return;
+            }
+            break;
+        case TokenType::GREATER:
+            m_returns.push(d_left > d_right); return;
+        case TokenType::GREATER_EQUAL:
+            m_returns.push(d_left >= d_right); return;
+        case TokenType::LESS:
+            m_returns.push(d_left < d_right); return;
+        case TokenType::LESS_EQUAL:
+            m_returns.push(d_left <= d_right); return;
+        case TokenType::EQUAL_EQUAL:
+            m_returns.push(isEqual(left, right)); return;
+        case TokenType::BANG_EQUAL:
+            m_returns.push(!isEqual(left, right)); return;
+    }
+    m_returns.push("NIL"); return;
+} 
+
+bool Interpreter::isEqual(const LoxObject& a, const LoxObject& b){
+    if (std::get_if<std::string>(&a) && std::get<std::string>(a) == "NIL"){
+        if (std::get_if<std::string>(&b) && std::get<std::string>(b) == "NIL") return true;
+        return false;
+    }
+    return a == b;
+}
---
+++ Interpreter.hpp
@@ -0,0 +1,23 @@
+// for null objects, maybe create a separate null 
+// object that represents null (cant use nullptr or NULL)
+
+#include "ExpressionTypes.hpp"
+#include <variant>
+#include <memory>
+#include <stack>
+
+using LoxObject = std::variant<bool, double, std::string>;
+
+class Interpreter: public Visitor{
+    public:
+    std::stack<LoxObject> m_returns;
+    bool isTruthy(const LoxObject& obj);
+    bool isEqual(const LoxObject& a, const LoxObject& b);
+
+    LoxObject evaluate(std::shared_ptr<Expr> expr);
+
+    void visit(Literal& literal) override;
+    void visit(Grouping& grouping) override;
+    void visit(Unary& unary) override;
+    void visit(Binary& binary) override;
+};
