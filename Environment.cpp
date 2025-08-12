4a403f6b109ad411efb742101bd2224adde6a832
mannyjimen <manueljimenez0320@gmail.com>
2025-08-11 23:57:53 -0400

    Getting back into the groove after LC grind. I realized that there is no point in making an enclosing environment a unique_ptr, it is actually not good, because the interpreter has a shared_ptr that manages the global environment, and in this case that there is a block statement, this new statement needs to have a hold of the local environment, and this local environment has to have access to the global environment. Making the enclosing environment a shared_ptr means that both the interpreter and the local environment can have access to the global environment.

--- Environment.cpp
+++ Environment.cpp
@@ -1,5 +1,13 @@
 #include "Environment.hpp"
 
+Environment::Environment(){
+    m_enclosing = nullptr;
+}
+
+Environment::Environment(std::shared_ptr<Environment> enclosing){
+    m_enclosing = enclosing;
+}
+
 void Environment::define(const std::string& name, const LoxObject& value){
     values.insert({name, value});
 }
@@ -9,10 +17,19 @@ void Environment::assign(const Token& token, const LoxObject& value){
         values.at(token.m_lexeme) = value;
         return;
     }
+
+    if (m_enclosing != nullptr) {
+        m_enclosing->assign(token, value);
+        return;
+    }
+
     throw LoxRuntimeError(token, "Undefined variable '" + token.m_lexeme + "'.");
 }
 
 LoxObject Environment::get(const Token& name){
     if (values.count(name.m_lexeme)) return values.at(name.m_lexeme);
+
+    if (m_enclosing != nullptr) return m_enclosing->get(name);
+
     throw LoxRuntimeError(name, "Undefined variable '" + name.m_lexeme + "'.");
 }
--- Environment.hpp
+++ Environment.hpp
@@ -10,6 +10,14 @@
 
 class Environment{
     public:
+    //global scope environment constructor
+    Environment();
+    //local scope environment constructor, enclosing is the immediate surrounding scope's environment.
+    Environment::Environment(std::shared_ptr<Environment> enclosing);
+
+    std::shared_ptr<Environment> m_enclosing;
+
+
     void define(const std::string& name, const LoxObject& value);
     void assign(const Token& token, const LoxObject& value);
     LoxObject get(const Token& name);
